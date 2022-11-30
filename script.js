1c66bdee35c4828490ba5943814f058d487db9cf
ashan <indrajee79@gmail.com>
2022-11-30 22:59:01 +0530

    more gsap animations

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-gsap.from(".header", { duration: 1, y:'-100%', ease: 'bounce'})
+/* gsap.from(".header", { duration: 1, y:'-100%', ease: 'bounce'})
 
 gsap.from(".link", { duration: 1, opacity: 0, delay: 1, stagger: 0.5})
 
@@ -10,4 +10,9 @@ gsap.to(".footer", {duration: 1, y: 0, ease: 'elastic', delay: 2.5})
 gsap.fromTo('.button', { opacity: 0, scale: 0, rotation: 720 }, { duration: 1, delay: 3.5, opacity:1, scale: 1, rotation: 0 })
 
 const obj = { x: 0}
-gsap.to(obj, { duration: 2, x: 100, onUpdate: () => console.log(obj.x) })
+gsap.to(obj, { duration: 2, x: 100, onUpdate: () => console.log(obj.x) }) */
+
+const timeline = gsap.timeline({ defaults: {duration: 1}})
+timeline
+    .from('.header', { duration: 3, y:'-100%', ease: 'bounce' })
+    .from('.link', { opacity: 0, stagger: 0.5})
