8520b95fdd6fabec1bf1f3992ed37b96b49d1843
ashan <indrajee79@gmail.com>
2022-11-30 22:51:14 +0530

    assigining gsap to javascript

--- script.js
+++ script.js
@@ -7,4 +7,7 @@ gsap.from(".right", {duration: 1, x: '-100vw', delay: 1, ease: 'power2.in'})
 gsap.from(".left", {duration: 1, delay: 1.5, x: '-100%'})
 gsap.to(".footer", {duration: 1, y: 0, ease: 'elastic', delay: 2.5})
 
-gsap.fromTo('.button', { opacity: 0, scale: 0, rotation: 720 }, { opacity:1, scale: 1, rotation: 0})
+gsap.fromTo('.button', { opacity: 0, scale: 0, rotation: 720 }, { duration: 1, delay: 3.5, opacity:1, scale: 1, rotation: 0 })
+
+const obj = { x: 0}
+gsap.to(obj, { duration: 2, x: 100, onUpdate: () => console.log(obj.x) })
